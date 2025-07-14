9c519bca5f87268ffdbd543a9078c472b68151dd
Carlos Jardel <jardel.apr@gmail.com>
2025-07-14 11:42:04 -0300

    Corrige porta para Render

--- app.py
+++ app.py
@@ -21,5 +21,9 @@ def geocode():
     response = requests.get(url)
     return jsonify(response.json())
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
